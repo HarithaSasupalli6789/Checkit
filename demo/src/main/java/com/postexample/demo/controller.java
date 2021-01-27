366ec2f2b82a70b4a9a800b149fb400a1a663eb1
unknown <haritha3425@gmail.com>
2021-01-27 19:49:04 +0530

    commit it

---
+++ demo/src/main/java/com/postexample/demo/controller.java
@@ -0,0 +1,19 @@
+package com.postexample.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @PostMapping("/postbody")
+    public String postBody(@RequestBody String fullName) {
+    	System.out.print("hello world");
+        return "Hello " + fullName;
+    }
+}
+
+
+
+
